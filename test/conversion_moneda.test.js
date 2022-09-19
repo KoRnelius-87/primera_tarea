2cf8a125741814e91327267a0e5fa1619fb4a2fa
KoRnelius-87 <luis.armando.moran@gmail.com>
2022-09-18 21:29:53 -0700

    TestRed

---
+++ test/conversion_moneda.test.js
@@ -0,0 +1,30 @@
+const assert = require("assert");
+
+describe("Conversion de Moneda", function () {
+  it("Pesos a Dolares", function () {
+    let monto = 100;
+    let valor = 20;
+
+    let total = money_convert_division(monto, valor);
+    assert.equal(100, total);
+  });
+
+  it("Dolares a Pesos", function () {
+    let monto = 5;
+    let valor = 20;
+ 
+    let total = money_convert_x(monto, valor);
+    assert.equal(5, total); 
+  });
+});
+
+//RED
+function money_convert_division(monto, valor){
+    return monto / valor;
+}
+
+function money_convert_x(monto, valor){
+    return monto * valor;
+}
+
+//
